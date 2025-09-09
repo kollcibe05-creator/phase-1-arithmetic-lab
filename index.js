cadf6bee35d43345d72ef016cb563e00fc7dee0b
Collins Kibet <kollcibe05@gmail.com>
2025-09-09 08:34:35 +0300

    performing arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
+
+let random = Math.floor((Math.random() * 10) + 1)
+
+
+  
+let num3 = 25
+let num4 = 21
+let mod = 25%21
+console.log(mod)
+
+let max = Math.max(5,15,2,20)
